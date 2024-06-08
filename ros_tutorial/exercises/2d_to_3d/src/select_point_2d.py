388a10e8498478c67c4e9b1dfea97b72ff25b78b
Riccardo Franceschini <riccardo.franceschini@eurecat.org>
2024-06-08 12:02:13 +0200

    added 2d to 3d solutions

--- ros_tutorial/exercises/2d_to_3d/src/select_point_2d.py
+++ ros_tutorial/exercises/2d_to_3d/src/select_point_2d.py
@@ -9,29 +9,52 @@ import cv2
 
 class PointSelector:
     def __init__(self, image_topic, point_topic):
+        self.bridge = CvBridge()
         # subcribe to the image topic
         self.image_sub = rospy.Subscriber(image_topic, Image, self.image_callback)
         # create a point publisher
         # Point is a message type that contains 3 float32 values: x, y, z
-        self.point_pub = rospy.Publisher(point_topic, Point, queue_size=1)
         
+        self.point_pub = rospy.Publisher(point_topic, Point, queue_size=1)
+
+        self.image = None
+        self.image_set = False
+        self.x_clicked = 0
+        self.y_clicked = 0
 
     def image_callback(self, msg):
-        #callback function for the image subscriber
+        self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        self.image_set = True
 
     def run(self):
-        # main function in which the image is displayed and the mouse callback is set
+        while not rospy.is_shutdown():
+            if self.image_set:
+                clicked_color = [int(self.y_clicked*self.image.shape[0]), int(self.x_clicked*self.image.shape[1])]
+                cv2.circle(self.image, (clicked_color[1],clicked_color[0]), 5, (0, 0, 255), -1)
+                cv2.imshow("image", self.image)
+                cv2.setMouseCallback("image", self.mouse_callback)
+                cv2.waitKey(1)
+            else:
+                rospy.loginfo("Waiting for data")
+                rospy.sleep(1)
 
     def mouse_callback(self, event, x, y, flags, param):
-        # mouse callback function that is called when the user clicks on the image
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.x_clicked = x / self.image.shape[1]
+            self.y_clicked = y / self.image.shape[0]
+            point = Point()
+            point.x = self.x_clicked
+            point.y = self.y_clicked
+            self.point_pub.publish(point)
+            rospy.loginfo("Clicked point: x {} y {}".format(point.x, point.y))
 
 
 if __name__ == "__main__":
     rospy.init_node("point_selector")
 
-    # set the topic names from the launch file
-    image_topic_ = 
-    point_topic_ = 
+    # read parameters from launch file
+    image_topic_ = rospy.get_param("~image_topic", "/camera/color/image")
+    point_topic_ = rospy.get_param("~point_topic", "/clicked_point")
 
     point_query = PointSelector(image_topic_, point_topic_)
     point_query.run()
